8b01cae8a8055428cbef029dcd7b042588041dc9
Aaron Diable <aaron.diable@gmail.com>
2020-02-05 21:04:50 +0000

    Random Password logic completed and about page added

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
 # Create your views here.
 
 def home(req):
@@ -7,4 +9,32 @@ def home(req):
 
 
 def password(req):
-    return render(req, 'generator/password.html')
+
+    generated_password = ''
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = int(req.GET.get('length', 14))
+
+
+    if req.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+
+    if req.GET.get('special'):
+        characters.extend(list('!@€£#$%^&*()?/.<,~`±§'))
+
+
+    if req.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+
+    return render(req, 'generator/password.html', {
+        'password':  generated_password
+    })
+
+
+def about(req):
+    return render(req, 'generator/about.html')
