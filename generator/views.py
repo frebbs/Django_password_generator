f3b47540b3bcf2f2b4720ba99e741af63ed492b1
Aaron Diable <aaron.diable@gmail.com>
2020-02-05 20:08:09 +0000

    Password generation page

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(req):
+    return render(req, 'generator/home.html')
+
+
+def password(req):
+    return render(req, 'generator/password.html')
